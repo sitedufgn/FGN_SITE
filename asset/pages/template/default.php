debd1b48816ef8f07e4149eb5b26126b49c220f2
Jules .M <julesdu45@gmail.com>
2015-08-19 12:08:26 +0200

    mise a jour footer du site

--- asset/pages/template/default.php
+++ asset/pages/template/default.php
@@ -1,6 +1,7 @@
  <!DOCTYPE html>
   <html>
     <head>
+    	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="asset/css/materialize.min.css"  media="screen,projection"/>
 
@@ -9,9 +10,34 @@
     </head>
 
     <body>
+
     	
     	<?= $content ?>
-    	
+
+
+    	    <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">FGN_STUDIO</h5>
+                <p class="grey-text text-lighten-4">Le site officiel du FGN_STUDIO</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Lien</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Youtube</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Connection</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright FGN_STUDIO
+            </div>
+          </div>
+        </footer>
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="asset/js/materialize.min.js"></script>
     </body>
