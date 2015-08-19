300d70e50825dbbfe38e0a92038718ff1b33587c
Jules .M <julesdu45@gmail.com>
2015-08-19 11:59:59 +0200

    Doosier image + bannier FGN

--- asset/pages/template/default.php
+++ asset/pages/template/default.php
@@ -9,14 +9,9 @@
     </head>
 
     <body>
-
-
+    	
     	<?= $content ?>
-
-
-
-
-
+    	
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="asset/js/materialize.min.js"></script>
     </body>
