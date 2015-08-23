3bea71b2014af00199656fc929141c169b4a7824
Jules Guesnon <krokilexjules@gmail.com>
2015-08-23 14:47:24 +0200

    Ajout code php pour conneion + ajout page de connexion

--- asset/pages/php/connexion.php
+++ asset/pages/php/connexion.php
@@ -1,10 +1,9 @@
 <?php 
-  session_start(); 
 
 if(isset($_POST['connexion'])) { 
     
     if(empty($_POST['pseudo'])) {
-        echo "Le champ Pseudo est vide.";
+        header('Location: asset/pages/connexion.php')
     } else {
       
         if(empty($_POST['mot_de_passe'])) {
@@ -36,24 +35,3 @@ if(isset($_POST['connexion'])) {
     }
 }
 ?>
-<h1>CONNEXION</h1>
-<div class="row">
-    <form class="col s12" action="?p=contact" method="GET">
-      <div class="row">
-        <div class="input-field col s6">
-          <input id="first_name" type="text" class="validate" name="FN">
-          <label for="first_name">Login</label>
-        </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate"name="LN">
-          <label for="last_name">Mot de passe</label>
-        </div>
-      </div>
-      <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-    <i class="material-icons">send</i>
-  </button>
-    </form>
-   
-        
-  </div>
-<br><br>
